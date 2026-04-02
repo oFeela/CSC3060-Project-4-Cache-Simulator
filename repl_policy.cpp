7f85353b48aec005935f0073c5a3cc3b5dd54eae
Feela <bryan.edelson@gmail.com>
2026-04-02 16:07:10 +0800

    Task 1 Person B completed

--- repl_policy.cpp
+++ repl_policy.cpp
@@ -5,24 +5,61 @@
 // Implement LRU first, then extend with SRRIP / BIP.
 // =========================================================
 
+/**
+ * In the case of a cache hit (is in the cache),
+ * mark the specified cache line as most recently used.
+ */
 void LRUPolicy::onHit(std::vector<CacheLine>& set, int way, uint64_t cycle) {
-    (void)set;
-    (void)way;
-    (void)cycle;
-    // TODO: mark the hit line as most recently used.
+    if (way >= set.size()) return;
+    set[way].last_access = cycle;
+    
+    // (void)set;
+    // (void)way;
+    // (void)cycle;
+    // // TODO: mark the hit line as most recently used.
 }
 
+/**
+ * In the case of a cache miss (not in the cache), 
+ * initialize a new cache line in the set for it.
+ */
 void LRUPolicy::onMiss(std::vector<CacheLine>& set, int way, uint64_t cycle) {
-    (void)set;
-    (void)way;
-    (void)cycle;
-    // TODO: initialize a newly inserted line as MRU.
+    if (way >= set.size()) return;
+    set[way].last_access = cycle;
+
+    // (void)set;
+    // (void)way;
+    // (void)cycle;
+    // // TODO: initialize a newly inserted line as MRU.
 }
 
+/**
+ * Get the least (oldest) recently used cache line 
+ * indicated by the last access cycle timestamp.
+ */
 int LRUPolicy::getVictim(std::vector<CacheLine>& set) {
-    (void)set;
-    // TODO: return the least recently used way.
-    return 0;
+    // always prioritize invalid cache line
+    // find the one with min last access
+    // on equal scenario, prioritize the invalid one
+
+    uint64_t min_last_access = UINT64_MAX;
+    int ans_index = 0;
+
+    for (size_t i = 0; i < set.size(); i++) {
+        CacheLine& line = set[i];
+        if (!line.valid) return i;
+        if (line.last_access < min_last_access) {
+            min_last_access = line.last_access;
+            ans_index = i;
+        }
+    }
+
+    return ans_index;
+
+    // (void)set;
+    // // TODO: return the least recently used way.
+
+    // return 0;
 }
 
 void SRRIPPolicy::onHit(std::vector<CacheLine>& set, int way, uint64_t cycle) {
